77a1793825dabe55099cd50d3a4377d11c0ada4e
RamónMN <ramon.martinez.nieto@gmail.com>
2021-08-20 19:11:32 +0200

    add new question with transaction

--- Service/QuestionServicesMySql.cs
+++ Service/QuestionServicesMySql.cs
@@ -16,9 +16,53 @@ namespace SimpreRestApiQuestions.Service
             connection = ConnectionDataBase.Instance();
         }
 
-        public void CreateQuestion(string question, string[] wrongAnswers, string correctAnswerd)
+        public void CreateQuestion(string question, string[] wrongAnswers, string correctAnswer)
         {
-            throw new NotImplementedException();
+            string query = $"INSERT INTO question (question, correct_answer) VALUES(@question, @correctAnswer); select last_insert_id();";
+            string queryWrongAnswers = $"INSERT INTO wrong_answer (id_question, wrong_one, wrong_two, wrong_three, wrong_four) " +
+                $"VALUES(@id_question, @wrong_one, @wrong_two, @wrong_three, @wrong_four); ";
+
+            MySqlTransaction transaction = null;
+
+            try
+            {
+                connection.Connect();
+                transaction = connection.Connection.BeginTransaction();
+
+                MySqlCommand cmd = connection.Connection.CreateCommand();
+                cmd.Transaction = transaction;
+                cmd.Connection = connection.Connection;
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@question", question);
+                cmd.Parameters.AddWithValue("@correctAnswer", correctAnswer);
+                int idQuestion = Convert.ToInt32(cmd.ExecuteScalar());
+
+                cmd.CommandText = queryWrongAnswers;
+                cmd.Parameters.AddWithValue("@id_question", idQuestion);
+                cmd.Parameters.AddWithValue("@wrong_one", wrongAnswers[0]);
+                cmd.Parameters.AddWithValue("@wrong_two", wrongAnswers[1]);
+                cmd.Parameters.AddWithValue("@wrong_three", wrongAnswers[2]);
+                cmd.Parameters.AddWithValue("@wrong_four", wrongAnswers[3]);
+                cmd.ExecuteNonQuery();
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    if(transaction != null) transaction.Rollback();
+                }
+                catch (MySqlException mySqlEx) 
+                {
+                    throw new Exception("Error during the insert transaction rollback.", mySqlEx);
+                }
+                throw new Exception("Fail in inerting new question. ", ex);
+            }
+            finally
+            {
+                CloseConnection();
+            }
         }
 
         public bool DeleteQuestion(int id)
@@ -31,11 +75,11 @@ namespace SimpreRestApiQuestions.Service
                 int rowsDeleted = cmd.ExecuteNonQuery();
                 return rowsDeleted > 0;
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 throw new Exception("Fail in DeleteQuestion class QuestionServiceMySql. ", ex);
             }
-            finally 
+            finally
             {
                 CloseConnection();
             }
@@ -47,8 +91,8 @@ namespace SimpreRestApiQuestions.Service
             {
                 string query = $"SELECT q.*, w.wrong_one, w.wrong_two, w.wrong_three, w.wrong_four FROM question q JOIN wrong_answer w ON q.id = w.id_question;";
                 connection.Connect();
-                MySqlCommand cmd = new MySqlCommand(query, connection.Connection);
-                var reader = cmd.ExecuteReader();
+                using MySqlCommand cmd = new MySqlCommand(query, connection.Connection);
+                using MySqlDataReader reader = cmd.ExecuteReader();
 
                 List<QuestionDto> listQuestions = new List<QuestionDto>();
                 while (reader.Read())
@@ -69,11 +113,11 @@ namespace SimpreRestApiQuestions.Service
                 }
                 return listQuestions.AsEnumerable();
             }
-            catch (Exception ex) 
-            { 
-                throw new Exception("Fail in GetAllQuestions class QuestionServiceMySql. ", ex); 
+            catch (Exception ex)
+            {
+                throw new Exception("Fail in GetAllQuestions class QuestionServiceMySql. ", ex);
             }
-            finally 
+            finally
             {
                 CloseConnection();
             }
@@ -85,8 +129,8 @@ namespace SimpreRestApiQuestions.Service
             try
             {
                 connection.Connect();
-                MySqlCommand cmd = new MySqlCommand(query, connection.Connection);
-                MySqlDataReader reader = cmd.ExecuteReader();
+                using MySqlCommand cmd = new MySqlCommand(query, connection.Connection);
+                using MySqlDataReader reader = cmd.ExecuteReader();
 
                 reader.Read();
                 return new QuestionDto
@@ -102,12 +146,13 @@ namespace SimpreRestApiQuestions.Service
                         reader.GetString("wrong_four")
                     }
                 };
+
             }
             catch (Exception ex)
             {
                 throw new Exception("Fail GetQuestion(int id) in QuestionServicesMySql method", ex);
             }
-            finally 
+            finally
             {
                 CloseConnection();
             }
@@ -120,7 +165,21 @@ namespace SimpreRestApiQuestions.Service
 
         public int maxQuestionsToRequest()
         {
-            throw new NotImplementedException();
+            string query = "select count(*) from question;";
+            try
+            {
+                connection.Connect();
+                using MySqlCommand cmd = new MySqlCommand(query, connection.Connection);
+                return int.Parse(cmd.ExecuteScalar().ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fail maxQuestionsToRequest method in QuestionServicesMySql", ex);
+            }
+            finally
+            {
+                CloseConnection();
+            }
         }
 
         private void CloseConnection()
